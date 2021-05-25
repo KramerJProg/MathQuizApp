50323ddf810648806e7c53ec4496dbfed937d0da
KramerJProg <kramerjensend@gmail.com>
2021-05-25 08:56:43 -0700

    Added another activity page for quiz questions.

---
+++ app/src/main/java/com/example/mathquizapplication/QuizQuestionScreen.java
@@ -0,0 +1,14 @@
+package com.example.mathquizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizQuestionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_question_screen);
+    }
+}
