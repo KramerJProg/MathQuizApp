d692ff084300eb61d39359e767dc9d2eaf163ac7
KramerJProg <kramerjensend@gmail.com>
2021-05-25 20:19:34 -0700

    Created QuestionDb to store questions via SQLite with getters and setters (None in yet). Created DbHelper class mainly for constants.

---
+++ app/src/main/java/com/example/mathquizapplication/DbHelper.java
@@ -0,0 +1,19 @@
+package com.example.mathquizapplication;
+
+import android.provider.BaseColumns;
+
+public final class DbHelper {
+
+    private DbHelper() {}
+
+    public static class QuestionsStorage implements BaseColumns {
+
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_NAME = "question";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_OPTION4 = "option4";
+        public static final String COLUMN_ANSWER_NMBR = "answer_nmbr";
+    }
+}
