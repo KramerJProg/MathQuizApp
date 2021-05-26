2547567745029906d9cce31befd885e040a37b32
KramerJProg <kramerjensend@gmail.com>
2021-05-25 18:57:21 -0700

    Wired Start Quiz button to move from Intro page to Questions page. Also changed radioBtn colors to dark_yellow.

--- app/src/main/java/com/example/mathquizapplication/IntroScreen.java
+++ app/src/main/java/com/example/mathquizapplication/IntroScreen.java
@@ -2,7 +2,10 @@ package com.example.mathquizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroScreen extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class IntroScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_screen);
+
+        Button startQuizBtn = findViewById(R.id.startQuizBtn);
+        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quizStart();
+            }
+        });
+    }
+
+    private void quizStart() {
+        Intent intent = new Intent(IntroScreen.this, QuizQuestionScreen.class);
+        startActivity(intent);
     }
 }
